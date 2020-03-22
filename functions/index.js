233ce69ce6b99ee98b06bd9f7dc985d22fa2a7ab
alextrujillo <alexandro4v@gmail.com>
2020-03-22 12:32:18 -0600

    carusel aghregado

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,10 @@ const {
     Suggestions,
     BasicCard,
     Button,
+    Image,
     SimpleResponse,
+    BrowseCarousel,
+    BrowseCarouselItem
 } = require('actions-on-google');
 
 /** Dialogflow Context Lifespans {@link https://dialogflow.com/docs/contexts#lifespan} */
@@ -19,6 +22,30 @@ const app = dialogflow({
 
 app.intent('Default Welcome Intent', (conv) => {
     conv.ask("Hola desde Webhook!");
+    conv.ask(new BrowseCarousel({
+        items: [
+            new BrowseCarouselItem({
+                title: 'Title of item 1',
+                url: 'https://example.com',
+                description: 'Description of item 1',
+                image: new Image({
+                    url: 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png',
+                    alt: 'Image alternate text',
+                }),
+                footer: 'Item 1 footer',
+            }),
+            new BrowseCarouselItem({
+                title: 'Title of item 2',
+                url: 'https://example.com',
+                description: 'Description of item 2',
+                image: new Image({
+                    url: 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png',
+                    alt: 'Image alternate text',
+                }),
+                footer: 'Item 2 footer',
+            }),
+        ],
+    }));
 });
 
 // The entry point to handle a http request
