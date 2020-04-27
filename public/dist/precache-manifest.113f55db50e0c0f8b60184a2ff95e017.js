108bdbd07e751254a737b9846d7eee2defdf7d5b
alextrujillo <alexandro4v@gmail.com>
2020-04-26 20:27:11 -0500

    Commit

---
+++ public/dist/precache-manifest.113f55db50e0c0f8b60184a2ff95e017.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4993a5e22e22e9613d2",
+    "url": "/css/app.7adfd1a2.css"
+  },
+  {
+    "revision": "b2eb8039c5a448125093",
+    "url": "/css/chunk-vendors.a862f514.css"
+  },
+  {
+    "revision": "2dcce27160495d68abf4945acd282448",
+    "url": "/fonts/materialdesignicons-webfont.2dcce271.woff"
+  },
+  {
+    "revision": "3e2c1c7919fb45d5dee5b0703fe52931",
+    "url": "/fonts/materialdesignicons-webfont.3e2c1c79.eot"
+  },
+  {
+    "revision": "a323c28ecd42189e70efca274f5b7089",
+    "url": "/fonts/materialdesignicons-webfont.a323c28e.woff2"
+  },
+  {
+    "revision": "e7dec9c5e1bd830c084f2d2fb94fa1e7",
+    "url": "/fonts/materialdesignicons-webfont.e7dec9c5.ttf"
+  },
+  {
+    "revision": "430e0fb8c85fc9e0073542939f4709df",
+    "url": "/img/typing.430e0fb8.svg"
+  },
+  {
+    "revision": "a5163e15e3029ee16347544e8119ec98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4993a5e22e22e9613d2",
+    "url": "/js/app.39ff41d3.js"
+  },
+  {
+    "revision": "b2eb8039c5a448125093",
+    "url": "/js/chunk-vendors.0fdb1b3e.js"
+  },
+  {
+    "revision": "182a8f0fc135e7a6f6db804a4bb141e3",
+    "url": "/logo.webp"
+  },
+  {
+    "revision": "13405e04714a87d4aa9838707c318d88",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- public/src/main.js
+++ public/src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
-
+import '@mdi/font/css/materialdesignicons.css'
 Vue.use(Vuetify, {
   iconfont: 'mdi'
 })
